5fe8a3f777c95111d3bfbc3b33a31dd72060bd5b
James Maxwell <james@dxetech.com>
2018-02-02 14:16:24 -0500

    added demo applications

---
+++ demos/SentryWeb/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SentryWeb.Models;
+using Serilog;
+
+namespace SentryWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            // Explicitly call our error logger
+            Log.Error("Intentional error logged at {TimeStamp}", DateTime.Now.ToLongTimeString());
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            // Demonstrate that Sentry will capture uncaught exceptions
+            DivByZero();
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        static int DivByZero()
+        {
+            var i = 0;
+            var j = 1 / i;
+            return j;
+        }
+    }
+}
---
+++ demos/SentryWeb/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Sinks.Sentry;
+using Serilog.Sinks.Sentry.AspNetCore;
+
+namespace SentryWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                // Insert Sentry DSN here
+                .WriteTo.Sentry("")
+                .Enrich.FromLogContext()
+                // Add Http Context for Sentry
+                .Destructure.With<HttpContextDestructingPolicy>()
+                .Filter.ByExcluding(e => e.Exception?.CheckIfCaptured() == true)
+                .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            // Add Http Context for Sentry
+            app.AddSentryContext();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
